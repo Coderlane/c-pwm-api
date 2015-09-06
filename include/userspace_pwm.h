d4fba50793280a18aafba457a9477f7e2e43b0b6
Travis Lane <travis.lane.osx@gmail.com>
2015-09-05 23:05:47 -0700

    Adding author tags

---
+++ include/userspace_pwm.h
@@ -0,0 +1,8 @@
+/**
+ * @file userspace_pwm.h
+ * @brief 
+ * @author Travis Lane
+ * @version 0.0.1
+ * @date 2015-09-05
+ */
+
--- src/userspace_pwm.c
+++ src/userspace_pwm.c
@@ -1,3 +1,10 @@
+/**
+ * @file userspace_pwm.c
+ * @brief 
+ * @author Travis Lane
+ * @version 0.0.1
+ * @date 2015-09-05
+ */
 
 
 #include <libudev.h>
